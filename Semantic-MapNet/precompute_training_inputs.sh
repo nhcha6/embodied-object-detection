f2cfa27a803cd5d1d361f349aaa24f1f1aabecb3
nhcha6 <nic_chapman@yahoo.com.au>
2024-03-05 15:02:07 +1100

    deleted semantic mapnet folder

--- Semantic-MapNet/precompute_training_inputs.sh
+++
@@ -1,4 +0,0 @@
- python precompute_training_inputs/build_data.py
- python precompute_training_inputs/build_crops.py
- python precompute_training_inputs/build_projindices.py
- python precompute_training_inputs/build_projindices_2.py
--- Semantic-MapNet/projector/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import _transform3D
-from .point_cloud import PointCloud
-from .projector import Projector
